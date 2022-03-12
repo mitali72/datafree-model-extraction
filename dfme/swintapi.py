4ee5942a3071e5a5ff815adc131d97b865d90b31
Mitali Meratwal <mitalimeratwal@gmail.com>
2022-03-12 23:22:27 +0530

    fixed swinTapi

--- dfme/swintapi.py
+++ dfme/swintapi.py
@@ -2,6 +2,7 @@ from mmaction.core import OutputHook
 from mmcv.parallel import scatter
 from operator import itemgetter
 from mmaction.apis import init_recognizer, inference_recognizer 
+import torch
 
 class SwinT():
 	def __init__(self, device):
@@ -9,21 +10,21 @@ class SwinT():
 		config_file = 'checkpoints/swinT/swin_tiny_patch244_window877_kinetics400_1k.py' #for swin-T
 		checkpoint_file = 'checkpoints/swin_tiny_patch244_window877_kinetics400_1k.pth'
 		self.device = torch.device(device)
-		model = init_recognizer(config_file, checkpoint_file, device=device)
-		model.eval()
+		self.model = init_recognizer(config_file, checkpoint_file, device=device)
+		self.model.eval()
 
 	def SwinTApi(self, data, as_tensor= True):
-	  '''data is a tensor: nxtxcxhxw'''
-	  data = data.permute(0,2,1,3,4)
-	  if next(model.parameters()).is_cuda:
-	      # scatter to specified GPU
-	      data = scatter(data, [device])[0]
+		'''data is a tensor: nxtxcxhxw'''
+		data = data.permute(0,2,1,3,4)
+		if next(self.model.parameters()).is_cuda:
+		# scatter to specified GPU
+			data = scatter(data, [self.device])[0]
 
-	  # forward the model
-	  with OutputHook(model, outputs=outputs, as_tensor=as_tensor) as h:
-	      with torch.no_grad():
-	          scores = model(data,return_loss=False)[0]
-	      return h.layer_outputs if outputs else None
+		# forward the model
+		with OutputHook(self.model, outputs=True, as_tensor=as_tensor) as h:
+			with torch.no_grad():
+				scores = self.model(data,return_loss=False)[0]
+			return scores
 
 	def __call__(self, x):
 		ans = self.SwinTApi(x)
