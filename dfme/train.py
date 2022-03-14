8e641b68845a000eb2c936d077295af7bc613a6c
cybershiptrooper <cybershiptrooper@gmail.com>
2022-03-14 12:58:57 +0530

    minor bugfix

--- dfme/train.py
+++ dfme/train.py
@@ -113,7 +113,7 @@ def train(args, teacher, student, generator, device, optimizer, epoch):
                 t_logit = t_logit.to(device)
             else:
                 t_logit = torch.zeros(args.batch_size,args.num_classes).to(device)
-                for i in range(N):
+                for i in range(args.batch_size):
                     t_logit[i] = teacher(fake[i].unsqueeze(0)).to(device)
                 # t_logit = teacher(fake)
             # Correction for the fake logits
